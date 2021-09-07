755637adadaad9c57e16df22762bfe959880e6a9
Saber <2282514478@qq.com>
2021-09-07 23:30:38 +0800

    在写集合案例

---
+++ src/A1_MyJava/基础知识/集合/集合案例/DouDiZu.java
@@ -0,0 +1,17 @@
+package A1_MyJava.基础知识.集合.集合案例;
+
+public class DouDiZu {
+
+    //启动
+    public static void main(String[] args) {
+        new DouDiZu().init();
+    }
+
+    //初始化
+    public void init(){
+
+        //创建
+
+    }
+
+}
