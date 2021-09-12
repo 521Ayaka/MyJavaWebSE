442e50ac4e2e76680e337fcf4a4c74ab073349b1
尴尬酱 <2282514478@qq.com>
2021-09-12 18:15:37 +0800

    学习主目录///在学I/O流>>>File类>>>删除操作

---
+++ src/A1_MyJava/基础知识/IO流/File类/File删除功能/FileDelete.java
@@ -0,0 +1,36 @@
+package A1_MyJava.基础知识.IO流.File类.File删除功能;
+
+//public boolean delete(){//...}   删除由此抽象路径名表示的文件或目录
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileDelete {
+
+    public static void main(String[] args) throws IOException {
+
+        //创建目录
+        File file1 = new File("D:\\源代码\\src\\A1_MyJava\\基础知识\\IO流\\File类\\File删除功能\\测试文件\\文件1.txt");
+        File file2 = new File("D:\\源代码\\src\\A1_MyJava\\基础知识\\IO流\\File类\\File删除功能\\测试文件\\目录1");
+
+        File file3 = new File("D:\\源代码\\src\\A1_MyJava\\基础知识\\IO流\\File类\\File删除功能\\测试文件\\不存在的文件.txt");
+
+        //使用delete方法进行删除抽象路径
+        System.out.println(file1.delete());
+        System.out.println(file2.delete());
+        System.out.println(file3.delete());
+
+        System.out.println("==============================");
+
+        //默认创建是在项目目录下的
+        new File("创建文件.txt").createNewFile();
+
+        //可追加到模块下
+        new File("src\\创建模块下的文件.txt").createNewFile();
+
+        //同样的，删除也是这样的，查看 恢复类 [ File_Redo ] 进行恢复操作
+
+
+    }
+
+}
--- src/A1_MyJava/基础知识/IO流/标准IO流.md
+++ src/A1_MyJava/基础知识/IO流/标准IO流.md
@@ -2,17 +2,19 @@
 
 ### File类
 
-path                  路径
+path                          路径
 
-parent              父路径
+parent                      父路径
 
-child                 子路径
+child                         子路径
 
-directory          目录
+isDirectory              是否目录
 
-exists                 存在
+isFile                         是否文件
 
-AbsolutePath   绝对路径
+exists                        存在
 
-​           
+getAbsolutePath    获取绝对路径
+
+Delete                       删除
 
