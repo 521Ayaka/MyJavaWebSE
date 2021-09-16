ae36ddae50dc7bbfc0da9e625c2978221f17ff6f
尴尬酱 <2282514478@qq.com>
2021-09-16 09:27:50 +0800

    学习主目录///再学>>>IO流>>>字节缓存流>>>视频案例>优化

--- src/A1_MyJava/基础知识/IO流/字节缓冲流/缓冲流案例/DameBufferedIO_putStream.java
+++ src/A1_MyJava/基础知识/IO流/字节缓冲流/缓冲流案例/DameBufferedIO_putStream.java
@@ -4,159 +4,259 @@ import java.io.*;
 
 public class DameBufferedIO_putStream {
 
+    private static String IN = "D:\\源代码\\src\\A1_MyJava\\基础知识\\IO流\\字节缓冲流\\缓冲流案例\\视频文件\\雷神pv.mp4";
+    private static String OUT_OR_APPEND = "D:\\源代码\\src\\A1_MyJava\\基础知识\\IO流\\字节缓冲流\\缓冲流案例\\";
+    private static String OUT_OR_FILE = "\\雷神pv.mp4";
+
     public static void main(String[] args) {
 
         /*
         将文件夹 视频文件 下的 马老师.mp4复制到当前类文件夹对应的method文件下
         */
-
         //计算时间
         //调用方法
-        method1(); //运行结果: method1用时: 3409毫秒
-        method2(); //运行结果: method2用时: 167毫秒
-        method3(); //运行结果: method3用时: 106毫秒
-                   //再次运行结果可能不同，系统底层问题。
-        /*
-        method1(); 最慢
-        method2(); 快
-        method3(); 最快
+
+        /*使用缓冲流    单个字符读取*/
+        method1(); // method1用时: 658毫秒
+
+        /*不使用缓冲流  单个字符读取*/
+        method2(); // method1用时: 71230毫秒
+
+        /*使用缓冲流   使用byte[1024]读取写入*/
+        method3(); // method2用时: 31毫秒
+
+        /*不使用缓冲流  使用byte[1024]读取写入*/
+        method4(); // method1用时: 94毫秒
+
+        /*使用缓冲流    使用byte[1024*32]读取写入*/
+        method5(); // method3用时: 17毫秒
+
+        //再次运行结果可能不同，系统底层问题。
+
+        /**
+        结论：
+        使用byte[]数组  读取速度快 写入快
+        使用字节缓冲流   读取速度快 写入快
         */
 
 
     }
 
     //计算所用时间 开始
-    public static long go(){
+    public static long go() {
         return System.currentTimeMillis();
     }
+
     //计算所用时间 结束
-    public static long end(long go){
-        return System.currentTimeMillis()-go;
+    public static long end(long go) {
+        return System.currentTimeMillis() - go;
     }
 
 
-    //单个byte进行读取数据
-    public static void method1(){
+    //使用 [缓冲流] [单个] byte进行读取数据
+    public static void method1() {
         //创建
         BufferedInputStream bis = null;
         BufferedOutputStream bos = null;
         //开始计时
         long go = go();
 
-        try{
-            bis = new BufferedInputStream(new FileInputStream("D:\\源代码\\src\\A1_MyJava\\基础知识\\IO流\\字节缓冲流\\缓冲流案例\\视频文件\\马老师.mp4"));
-            bos = new BufferedOutputStream(new FileOutputStream("D:\\源代码\\src\\A1_MyJava\\基础知识\\IO流\\字节缓冲流\\缓冲流案例\\method1\\马老师.mp4"));
+        try {
+            bis = new BufferedInputStream(new FileInputStream(IN));
+            bos = new BufferedOutputStream(new FileOutputStream(String.valueOf(new StringBuilder(OUT_OR_APPEND).append("method1").append(OUT_OR_FILE))));
             //开始读取并写入数据
             int by;
-            while ( (by=bis.read()) != -1 ){
+            while ((by = bis.read()) != -1) {
                 bos.write(by);
             }
 
-        }catch(IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
-        }finally{
-            if (bis != null){
-                try{
+        } finally {
+            if (bis != null) {
+                try {
                     bis.close();//释放资源
-                }catch (IOException e){
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
-            if (bos != null){
-                try{
+            if (bos != null) {
+                try {
                     bos.close();//释放资源
-                }catch (IOException e){
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         }
         //结束计时
         long end = end(go);
-        System.out.println("method1用时: "+end+"毫秒");
+        System.out.println("method1用时: " + end + "毫秒");
+    }
+
+    //不使用 [缓冲流] [单个] byte进行读取数据
+    public static void method2() {
+        //创建
+        FileInputStream fis = null;
+        FileOutputStream fos = null;
+        //开始计时
+        long go = go();
+
+        try {
+            fis = new FileInputStream(IN);
+            fos = new FileOutputStream(String.valueOf(new StringBuilder(OUT_OR_APPEND).append("method2").append(OUT_OR_FILE)));
+            //开始读取并写入数据
+            int by;
+            while ((by = fis.read()) != -1) {
+                fos.write(by);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();//释放资源
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (fos != null) {
+                try {
+                    fos.close();//释放资源
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        //结束计时
+        long end = end(go);
+        System.out.println("method1用时: " + end + "毫秒");
     }
 
 
-    //使用byte[]数组进行读取数据 ，byte.length赋值为 [1024]
-    public static void method2(){
+    //使用字节缓冲流 使用byte[]数组进行读取数据 ，byte.length赋值为 [1024]
+    public static void method3() {
         //创建
         BufferedInputStream bis = null;
         BufferedOutputStream bos = null;
         //开始计时
         long go = go();
 
-        try{
-            bis = new BufferedInputStream(new FileInputStream("D:\\源代码\\src\\A1_MyJava\\基础知识\\IO流\\字节缓冲流\\缓冲流案例\\视频文件\\马老师.mp4"));
-            bos = new BufferedOutputStream(new FileOutputStream("D:\\源代码\\src\\A1_MyJava\\基础知识\\IO流\\字节缓冲流\\缓冲流案例\\method2\\马老师.mp4"));
+        try {
+            bis = new BufferedInputStream(new FileInputStream(IN));
+            bos = new BufferedOutputStream(new FileOutputStream(String.valueOf(new StringBuilder(OUT_OR_APPEND).append("method3").append(OUT_OR_FILE))));
             //开始读取并写入数据
-            byte [] array = new byte[1024];
+            byte[] array = new byte[1024];
             int len;
-            while ( (len=bis.read(array)) != -1 ){//byte[]构造方法
-                bos.write(array,0,len);//三参构造方法
+            while ((len = bis.read(array)) != -1) {//byte[]构造方法
+                bos.write(array, 0, len);//三参构造方法
             }
 
-        }catch(IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
-        }finally{
-            if (bis != null){
-                try{
+        } finally {
+            if (bis != null) {
+                try {
                     bis.close();//释放资源
-                }catch (IOException e){
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
-            if (bos != null){
-                try{
+            if (bos != null) {
+                try {
                     bos.close();//释放资源
-                }catch (IOException e){
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         }
         //结束计时
         long end = end(go);
-        System.out.println("method2用时: "+end+"毫秒");
+        System.out.println("method2用时: " + end + "毫秒");
     }
 
 
-    //使用byte[]数组进行读取数据 ，byte.length赋值为 [32*1024]
-    public static void method3(){
+    //不使用 [缓冲流] [使用byte[]数组] 进行读取数据
+    public static void method4() {
+        //创建
+        FileInputStream fis = null;
+        FileOutputStream fos = null;
+        //开始计时
+        long go = go();
+
+        try {
+            fis = new FileInputStream(IN);
+            fos = new FileOutputStream(String.valueOf(new StringBuilder(OUT_OR_APPEND).append("method4").append(OUT_OR_FILE)));
+            //开始读取并写入数据
+            byte[] array = new byte[1024];//长度为1024
+            int by;
+            while ((by = fis.read(array)) != -1) {
+                fos.write(array, 0, by);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();//释放资源
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (fos != null) {
+                try {
+                    fos.close();//释放资源
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        //结束计时
+        long end = end(go);
+        System.out.println("method1用时: " + end + "毫秒");
+    }
+
+
+    //使用字节缓冲流 使用byte[]数组进行读取数据 ，byte.length赋值为 [32*1024]
+    public static void method5() {
         //创建
         BufferedInputStream bis = null;
         BufferedOutputStream bos = null;
         //开始计时
         long go = go();
 
-        try{
-            bis = new BufferedInputStream(new FileInputStream("D:\\源代码\\src\\A1_MyJava\\基础知识\\IO流\\字节缓冲流\\缓冲流案例\\视频文件\\马老师.mp4"));
-            bos = new BufferedOutputStream(new FileOutputStream("D:\\源代码\\src\\A1_MyJava\\基础知识\\IO流\\字节缓冲流\\缓冲流案例\\method3\\马老师.mp4"));
+        try {
+            bis = new BufferedInputStream(new FileInputStream(IN));
+            bos = new BufferedOutputStream(new FileOutputStream(String.valueOf(new StringBuilder(OUT_OR_APPEND).append("method5").append(OUT_OR_FILE))));
             //开始读取并写入数据
-            byte [] array = new byte[1024*32];
+            byte[] array = new byte[1024 * 32];
             int len;
-            while ( (len=bis.read(array)) != -1 ){//byte[]构造方法
-                bos.write(array,0,len);//三参构造方法
+            while ((len = bis.read(array)) != -1) {//byte[]构造方法
+                bos.write(array, 0, len);//三参构造方法
             }
 
-        }catch(IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
-        }finally{
-            if (bis != null){
-                try{
+        } finally {
+            if (bis != null) {
+                try {
                     bis.close();//释放资源
-                }catch (IOException e){
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
-            if (bos != null){
-                try{
+            if (bos != null) {
+                try {
                     bos.close();//释放资源
-                }catch (IOException e){
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         }
         //结束计时
         long end = end(go);
-        System.out.println("method3用时: "+end+"毫秒");
+        System.out.println("method3用时: " + end + "毫秒");
     }
 
 
