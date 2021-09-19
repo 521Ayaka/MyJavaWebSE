f3bccfd5f768504745a9c127c1dd77f402904b0b
尴尬酱 <2282514478@qq.com>
2021-09-19 22:47:08 +0800

    学习主目录///再学>>>IO流>>>字符流>>>字符缓冲流

---
+++ src/A1_MyJava/基础知识/IO流/字符缓冲流/字符缓冲流案例/NewJavaCopy.java
@@ -0,0 +1,51 @@
+package A1_MyJava.基础知识.IO流.字符缓冲流.字符缓冲流案例;
+
+import java.io.*;
+/*
+继续改进【字符流案例】这次使用 [字符缓冲流] 来实现
+
+*/
+public class NewJavaCopy {
+
+    public static void main(String[] args) {
+
+        //创建字符缓冲流
+        BufferedReader br = null;
+        BufferedWriter bw = null;
+
+        try {
+            //赋值路径对象
+            br = new BufferedReader(new FileReader("D:\\源代码\\src\\A1_MyJava\\基础知识\\IO流\\字符缓冲流\\字符缓冲流案例\\NewJavaCopy.java"));
+            bw = new BufferedWriter(new FileWriter("D:\\源代码\\src\\A1_MyJava\\基础知识\\IO流\\字符缓冲流\\字符缓冲流案例\\复制到这里\\NewJavaCopy.java"));
+            //开始复制
+            char[] array = new char[1024];
+            int len;
+            while ((len = br.read(array)) != -1) {
+                //复制
+                bw.write(array, 0, len);
+                //打印输出到控制台
+                System.out.print(new String(array,0,len));
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br!=null) {
+                try {
+                    br.close();//释放资源
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (bw!=null) {
+                try {
+                    bw.close();//释放资源
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+}
---
+++ src/A1_MyJava/基础知识/IO流/字符缓冲流/字符缓冲流特有方法/BufferedRWProMethod.java
@@ -0,0 +1,11 @@
+package A1_MyJava.基础知识.IO流.字符缓冲流.字符缓冲流特有方法;
+
+public class BufferedRWProMethod {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
