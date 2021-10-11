822c71c17303e84e6bed7b2c91c9ab0301e5a86a
尴尬酱 <2282514478@qq.com>
2021-10-11 09:52:28 +0800

    学习主目录///  进程和线程 / 线程调度 与 线程控制

---
+++ src/A1_MyJava/基础知识/进程和线程/A2_线程调度/ThreadPriority.java
@@ -0,0 +1,11 @@
+package A1_MyJava.基础知识.进程和线程.A2_线程调度;
+
+public class ThreadPriority extends Thread{
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            System.out.println(getName() + ": " + i);
+        }
+    }
+}
