013b2ae56519981cc89d28139695900f58c8dc6e
尴尬酱 <2282514478@qq.com>
2021-10-14 11:43:14 +0800

    学习主目录///  进程和线程 / 线程安全与线程同步

---
+++ src/A1_MyJava/基础知识/进程和线程/A6_线程同步/B1_售票案例/MyRunnable.java
@@ -0,0 +1,24 @@
+package A1_MyJava.基础知识.进程和线程.A6_线程同步.B1_售票案例;
+
+public class MyRunnable implements Runnable {
+
+    private int tick = 100;//门票总数
+
+    @Override
+    public void run() {
+
+        while (true) {
+            if (tick > 0) {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread().getName() + "正在出售第:" + tick + "张票");
+                tick--;
+
+            }
+        }
+
+    }
+}
---
+++ src/A1_MyJava/基础知识/进程和线程/A6_线程同步/B3_同步方法/RunsMethod.java
@@ -0,0 +1,18 @@
+package A1_MyJava.基础知识.进程和线程.A6_线程同步.B3_同步方法;
+
+public class RunsMethod {
+
+    public static void main(String[] args) {
+        MyRunnable run = new MyRunnable();
+
+        Thread t11 = new Thread(run, "窗口1");
+        Thread t12 = new Thread(run, "窗口2");
+        Thread t13 = new Thread(run, "窗口3");
+
+        t11.start();
+        t12.start();
+        t13.start();
+
+    }
+
+}
