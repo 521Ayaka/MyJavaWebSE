5bf394247f3849ee448bfec48230b197b370ff5a
尴尬酱 <2282514478@qq.com>
2021-10-15 10:13:56 +0800

    学习主目录///  进程和线程 / 线程同步 和 Lock锁

---
+++ src/A1_MyJava/基础知识/进程和线程/A7_线程同步类/RunsLei.java
@@ -0,0 +1,43 @@
+package A1_MyJava.基础知识.进程和线程.A7_线程同步类;
+/*
+    线程安全的类:
+        StringBuffer
+        Vector
+        Hashtable
+
+*/
+
+import java.util.*;
+
+public class RunsLei {
+
+    public static void main(String[] args) {
+
+        StringBuffer sb1 = new StringBuffer();    //synchronized方法，线程安全的
+        StringBuilder sb2 = new StringBuilder();  //没有使用synchronized修饰
+
+        Vector<String> ve = new Vector<>();          //synchronized方法，线程安全的
+        ArrayList<String> list = new ArrayList<>();  //没有使用synchronized修饰
+
+        Hashtable<String, String> ht = new Hashtable<>(); //synchronized方法，线程安全的
+        HashMap<String, String> hm = new HashMap<>();     //没有使用synchronized修饰
+
+        //==========================================================================
+        /*
+        一般来说,线程同步时，使用StringBuffer、Vector、Hashtable三个类来操作
+        但是,Vector和Hashtable也不经常使用。
+        使用,Collections 中的方法 来操作
+                public static <T> List<T> synchronizedList(List<T> list)
+                public static <K,V> Map<K,V> synchronizedMap(Map<K,V> m)
+                ......
+        */
+
+        List<String> array = Collections.synchronizedList(new ArrayList<String>());
+        //array就是同步安全的了
+        Map<String, String> map = Collections.synchronizedMap(new HashMap<String, String>());
+        //map就是同步安全的了
+
+
+    }
+
+}
---
+++ src/A1_MyJava/基础知识/进程和线程/A8_Lock锁/MyRunnable.java
@@ -0,0 +1,66 @@
+package A1_MyJava.基础知识.进程和线程.A8_Lock锁;
+/*
+使用 ReentrantLock来实现Lock接口
+使用 lock()方法   上锁
+使用 unlock()方法 开锁
+
+使用 try{
+        lock();
+
+    }finally{
+        unlock();
+    }
+    代码块 进行加锁开锁 保证一定要开锁
+
+*/
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class MyRunnable implements Runnable {
+    private int tick = 100;
+    //创建Lock锁
+    private Lock lock = new ReentrantLock();
+
+    //重写run方法
+    @Override
+    public void run() {
+        while (true) {
+
+            /*
+            //上锁
+            lock.lock();//上锁
+            if (tick > 0) {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread().getName() + ":正在出售第" + tick + "张票");
+                tick--;
+            }
+            //开锁
+            lock.unlock();//开锁
+            */
+
+            //上锁
+            try {//如果这里面出现了问题,也不会影响finally里面的开锁
+                lock.lock();//上锁
+                if (tick > 0) {
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(Thread.currentThread().getName() + ":正在出售第" + tick + "张票");
+                    tick--;
+                }
+            } finally {//finally一定执行
+                //开锁
+                lock.unlock();//开锁
+            }
+
+
+        }
+    }
+}
